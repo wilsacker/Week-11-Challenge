72cddbf667a6c35d68263349dda68c4061884ab9
wilsacker <williamsuttona@gmail.com>
2024-09-21 00:20:09 -0700

    adding routes

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // Serve static assets from the "public" folder
 app.use(express.static('public'));
+
+// Route to serve the notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+  
+  // Default route to serve the index page
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
